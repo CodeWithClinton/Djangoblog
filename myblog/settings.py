4089a071255b97c4a70bfa87f937cec7e9e7071a
Clinton Nwachukwu <clintonchibunna16@gmail.com>
2021-04-03 06:19:11 -0700

    changes added

--- myblog/settings.py
+++ myblog/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -48,6 +49,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middles.WhiteNoiseMiddleware'
 ]
 
 ROOT_URLCONF = 'myblog.urls'
@@ -130,5 +132,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR/'static']
+STATIC_ROOT = BASE_DIR/'staticfiles'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 MEDIA_URL = '/media/'
 MEDIA_ROOT =  BASE_DIR/'media' 
+
+django_heroku.settings(locals())
